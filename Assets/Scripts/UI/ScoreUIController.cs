8be64051b684604691d57ec46a3c0248974507d0
Joseph <electronic@mailbox.org>
2023-07-31 14:55:03 -0400

    Display score in SampleScene.

---
+++ Assets/Scripts/UI/ScoreUIController.cs
@@ -0,0 +1,20 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Platformer.UI
+{
+    public class ScoreUIController : MonoBehaviour
+    {
+        public static TextMeshProUGUI CurrentScore;
+
+        public static void UpdateScoreText()
+        {
+            CurrentScore = GameObject.Find("Score").GetComponent<TextMeshProUGUI>();
+            if (CurrentScore != null)
+            {
+                CurrentScore.SetText($"Score: {Mechanics.Score.GetScore()}");
+            }
+        }
+
+    }
+}
