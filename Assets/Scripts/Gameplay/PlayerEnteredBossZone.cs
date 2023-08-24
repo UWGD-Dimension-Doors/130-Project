0bccd815be18e0f266f613c7d25f098f2b12fb0f
Joseph <electronic@mailbox.org>
2023-08-24 23:48:58 +0200

    Make boss big and scary, with a laugh and patrol pattern.

---
+++ Assets/Scripts/Gameplay/PlayerEnteredBossZone.cs
@@ -0,0 +1,32 @@
+using Platformer.Core;
+using Platformer.Mechanics;
+using Platformer.Model;
+
+namespace Platformer.Gameplay
+{
+
+    /// <summary>
+    /// This event is triggered when the player character enters a trigger with a BossZone component.
+    /// </summary>
+    /// <typeparam name="PlayerEnteredBossZone"></typeparam>
+    public class PlayerEnteredBossZone : Simulation.Event<PlayerEnteredBossZone>
+    {
+        public BossZone bossZone;
+
+        PlatformerModel model = Simulation.GetModel<PlatformerModel>();
+
+        public override void Execute()
+        {
+            PlayBossSound();
+        }
+
+        private void PlayBossSound()
+        {
+            if (model.player.audioSource && bossZone.bossAudio)
+            {
+                model.player.audioSource.PlayOneShot(bossZone.bossAudio);
+            }
+        }
+
+    }
+}
