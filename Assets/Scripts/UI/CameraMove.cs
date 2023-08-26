69270abf7e0edc22633519a4e00611de8a308715
MarZ4c <133806505+MarZ4c@users.noreply.github.com>
2023-08-25 21:13:41 -0700

    Title Screen horizontal pan

--- Assets/Scripts/UI/CameraMove.cs
+++ Assets/Scripts/UI/CameraMove.cs
@@ -8,6 +8,8 @@ public class CameraMove : MonoBehaviour
 
     public Vector3 moveSpeed = new Vector3(1,0,0);
 
+    private bool change;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +19,22 @@ public class CameraMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         transform.position = transform.position + moveSpeed;
+
+        if (transform.position.x > 9.290164f)
+        {
+            moveSpeed = moveSpeed * -1f;
+            change = true;
+        }
+
+        if (transform.position.x < -1.4f)
+        {
+            moveSpeed = moveSpeed * -1f;
+            change = false;
+        }
+
+       // transform.position = transform.position + moveSpeed;
         
 
     }
