a9be4ed761febb91e39efe35ee128dfe9e639627
Joseph <electronic@mailbox.org>
2023-08-27 16:04:06 +0200

    Add some game tests to check if scenes are enabled.

---
+++ Assets/Tests/Game.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using UnityEditor;
+
+public class GameTests
+{
+    private readonly string[] EnabledScenes = {"Title", "Seamount", "Victory"};
+
+    [Test]
+    public void TestExpectedScenesEnabled()
+    {
+        foreach (string SceneName in EnabledScenes)
+        {
+            Assert.True(GetSceneEnabled(SceneName));
+        }
+    }
+
+    [Test]
+    public void TestUnexpectedScenesNotEnabled()
+    {
+        string UnexpectedScene = "SampleScene";
+
+        Assert.False(GetSceneEnabled(UnexpectedScene));
+    }
+
+    // Helper function
+    private bool GetSceneEnabled(string sceneName)
+    {
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        {
+            if (scene.path.Contains(sceneName))
+            {
+                return scene.enabled;
+            }
+        }
+
+        return false;
+    }
+}
